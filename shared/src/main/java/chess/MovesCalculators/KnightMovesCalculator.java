e1d89b842110c16ed2003b37171046e899e29553
sdbills <>
2025-01-26 14:15:50 -0700

    KnightMoves

--- shared/src/main/java/chess/MovesCalculators/KnightMovesCalculator.java
+++ shared/src/main/java/chess/MovesCalculators/KnightMovesCalculator.java
@@ -4,12 +4,31 @@ import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class KnightMovesCalculator extends PiecesMovesCalculator{
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        return List.of();
+        var moves = new ArrayList<ChessMove>();
+        int[] directions = {-1,1};
+        for (var n : directions) {
+            for (var m : directions) {
+                for (var p : directions) {
+                    ChessPosition newPosition;
+                    if (p == 1) {
+                        newPosition = new ChessPosition(position.getRow()+2*n, position.getColumn()+m);
+                    } else {
+                        newPosition = new ChessPosition(position.getRow()+n, position.getColumn()+2*m);
+                    }
+                    var newMove = createLegalMove(board,position,newPosition);
+                    if (newMove != null) {
+                        moves.add(newMove);
+                    }
+                }
+            }
+        }
+        return moves;
     }
 }
