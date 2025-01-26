ea77f6a04bee1845b5b49872bb6fda37b819becb
sdbills <>
2025-01-26 14:25:43 -0700

    Rook,BishopMoves

--- shared/src/main/java/chess/MovesCalculators/BishopMovesCalculator.java
+++ shared/src/main/java/chess/MovesCalculators/BishopMovesCalculator.java
@@ -4,12 +4,29 @@ import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class BishopMovesCalculator extends PiecesMovesCalculator{
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        return List.of();
+        var newMoves = new ArrayList<ChessMove>();
+        int[] directions = {-1,1};
+        for (var n : directions) {
+            for (var m : directions) {
+                ChessPosition newPosition;
+                int i = 1;
+                do {
+                    newPosition = new ChessPosition(position.getRow()+n*i, position.getColumn()+m*i);
+                    var newMove = createLegalMove(board, position, newPosition);
+                    if (newMove != null) {
+                        newMoves.add(newMove);
+                    }
+                    i++;
+                } while (newPosition.isValid() && board.getPiece(newPosition) == null);
+            }
+        }
+        return newMoves;
     }
 }
--- shared/src/main/java/chess/MovesCalculators/RookMovesCalculator.java
+++ shared/src/main/java/chess/MovesCalculators/RookMovesCalculator.java
@@ -4,12 +4,34 @@ import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class RookMovesCalculator extends PiecesMovesCalculator{
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        return List.of();
+        var newMoves = new ArrayList<ChessMove>();
+        int[] directions = {-1,1};
+        for (var n : directions) {
+            for (var m : directions) {
+                ChessPosition newPosition;
+                int i = 1;
+                do {
+                    if (n == -1) {
+                        newPosition = new ChessPosition(position.getRow()+i*m, position.getColumn());
+                    } else {
+                        newPosition = new ChessPosition(position.getRow(), position.getColumn()+i*m);
+                    }
+                    var newMove = createLegalMove(board,position,newPosition);
+                    if (newMove != null) {
+                        newMoves.add(newMove);
+                    }
+                    i++;
+                } while (newPosition.isValid() && board.getPiece(newPosition) == null);
+            }
+        }
+
+        return newMoves;
     }
 }
